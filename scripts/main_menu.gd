4dbc182ddf60079c1007f76fee61ba96d675ae4c
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-07-10 18:36:08 +0200

    anti aliasing slider

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,12 +10,14 @@ extends Node3D
 
 @onready var anti_aliasing_text : Label = $Options/Control/VBoxContainer/Anti_aliasing_text
 
+var anti_aliasing_value = 100
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 	
 
 func _ready():
-	anti_aliasing_text.text = "Anti-Aliasing: " + str(ProjectSettings.get_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", "Value"))
+	pass
 
 func _on_options_pressed():
 	main_ui.visible = false
@@ -30,16 +32,20 @@ func _on_exit_pressed():
 func _on_h_slider_value_changed(value):
 	print(value)
 	if value == 25:
+		anti_aliasing_text.text = "Anti-Aliasing: Disabled"
 		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 1)
 		ProjectSettings.save()
 
 	if value == 50:
+		anti_aliasing_text.text = "Anti-Aliasing: 2x"
 		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 2)
 		ProjectSettings.save()
 	if value == 75:
+		anti_aliasing_text.text = "Anti-Aliasing: 4x"
 		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 3)
 		ProjectSettings.save()
 	if value == 100:
+		anti_aliasing_text.text = "Anti-Aliasing: 8x"
 		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 4)
 		ProjectSettings.save()
 		
