48a94f0022c459718f78e27949f9f1e744d85f25
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-07-10 18:16:55 +0200

    main menu tweaks

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,11 +8,14 @@ extends Node3D
 
 @onready var options : CanvasLayer = $Options
 
-
+@onready var anti_aliasing_text : Label = $Options/Control/VBoxContainer/Anti_aliasing_text
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 	
+
+func _ready():
+	anti_aliasing_text.text = "Anti-Aliasing: " + str(ProjectSettings.get_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", "Value"))
 
 func _on_options_pressed():
 	main_ui.visible = false
@@ -22,3 +25,21 @@ func _on_options_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_h_slider_value_changed(value):
+	print(value)
+	if value == 25:
+		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 1)
+		ProjectSettings.save()
+
+	if value == 50:
+		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 2)
+		ProjectSettings.save()
+	if value == 75:
+		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 3)
+		ProjectSettings.save()
+	if value == 100:
+		ProjectSettings.set_setting("Rendering/Anti Aliasing/Quality/MSAA 3D", 4)
+		ProjectSettings.save()
+		
