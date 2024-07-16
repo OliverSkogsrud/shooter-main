fdba96a1e6ffa02142bb7ea22821b1919baa4401
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-07-16 23:13:26 +0200

    started on making ai for enemySpider

---
+++ scripts/SpiderEnemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var ray = $goofyAhhMonster/Armature/Skeleton3D/BoneAttachment3D/RayCast3D
+
+var isSeeingPlayer = false
+
+@onready var animTree = $AnimationTree
+
+func animate():
+	if isSeeingPlayer == true:
+		pass #Run
+	else:
+		pass #idle
+
+func _physics_process(delta):
+	animate()
+	
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider.has_method("damage"):
+			isSeeingPlayer = true
+	else : isSeeingPlayer = false
