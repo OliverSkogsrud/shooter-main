f046a7e3368fb2c342191a07cd7abe0280787e52
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-07-17 14:13:52 +0200

    more work on spider ai

--- scripts/SpiderEnemy.gd
+++ scripts/SpiderEnemy.gd
@@ -8,19 +8,28 @@ const JUMP_VELOCITY = 4.5
 
 var isSeeingPlayer = false
 
-@onready var animTree : AnimationTree = $AnimationTree
+@onready var animation_player = $goofyAhhMonster/AnimationPlayer
 
 func animate():
 	if isSeeingPlayer == true:
-		pass #Run
+		var look_dir = -transform.basis.z
+		velocity = look_dir * SPEED
+		animation_player.play("Run")
 	else:
-		pass #idle
+		animation_player.play("Idle")
 
 func _physics_process(delta):
 	animate()
+	
+	if not is_on_floor():
+		velocity.y -= 1.5
 	
 	if ray.is_colliding():
 		var collider = ray.get_collider()
 		if collider.has_method("damage"):
 			isSeeingPlayer = true
 	else : isSeeingPlayer = false
+	
+func _process(delta):
+	look_at(-get_parent().get_node("Player").position)
+	await get_tree().create_timer(0.2).timeout
